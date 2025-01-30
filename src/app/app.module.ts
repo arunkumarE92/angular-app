e0729360a302bc503121f213e7bffa01a4b0cc89
Arunkumar <arunkumar.e@laderatechnology.com>
2025-01-30 15:11:23 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './home-page/home-page.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { InventoryComponent } from './home-page/inventory/inventory.component';
+import { OrdersComponent } from './home-page/orders/orders.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    InventoryComponent,
+    OrdersComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule
   ],
   providers: [
     provideClientHydration()
